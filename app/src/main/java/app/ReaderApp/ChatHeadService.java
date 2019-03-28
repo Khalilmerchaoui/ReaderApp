18be53dde08cc3f3d7d02ee336b2a1712ded65a5
ahmed khalil merchaoui <khalilmerchaoui@gmail.com>
2019-03-28 17:15:36 -0400

    Bug fix (TYPE_APPLICATION_OVERLAY)

--- app/src/main/java/app/ReaderApp/ChatHeadService.java
+++ app/src/main/java/app/ReaderApp/ChatHeadService.java
@@ -146,13 +146,13 @@ public class ChatHeadService extends Service {
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
         final WindowManager.LayoutParams FullScreenParams = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL| WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
         FullScreenParams.gravity = Gravity.RIGHT | Gravity.TOP;
@@ -354,7 +354,6 @@ public class ChatHeadService extends Service {
         windowManager.addView(fab_layout, params);
 
     }
-
 
   private void showPopupWindow(View view) {
         PopupMenu popup = new PopupMenu(ChatHeadService.this, view);
